fa78bc5e733b5175e41f2dcc8969701f938c8afe
EVogel1999 <eav990@gmail.com>
2019-04-30 09:51:33 -0400

    Change origin

--- src/app.ts
+++ src/app.ts
@@ -23,7 +23,7 @@ function startServer(agility: AgilityDatastore) {
 
   // Needed to be able to connect to the client
   app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Origin', 'http://www.agile-projects.com');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,projectid,sprintid,authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
